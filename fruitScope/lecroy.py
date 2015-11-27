677bfe788a1b71f3b55c49015b1b7f146ea902c3
Sun Yudong <sunjerry019@gmail.com>
2015-11-27 09:23:00 +0800

    added code for config file

--- fruitScope/lecroy.py
+++ fruitScope/lecroy.py
@@ -8,22 +8,47 @@ import json
 import random
 import argparse
 
+class _:
+    def isInt(self, x):
+        try:
+            float(x)
+            return True
+        except ValueError:
+            return False
+
 class lecroy():
     def __init__(self):
-        cfg = {}
-        with open('./cfg/', 'r') as f:
-            # get the configuration settings, only the port for now
-            x = f.read()
-            x = x.split('\n')
-            for i in x:
-                s = i.rstrip().split('=')
-                try:
-                    cfg[s[0]] = s[1]
-                    if _isInt(s[1]):
-                        cfg[s[0]] = int(s[1])
-                except IndexError:
-                    pass
-        print "Loaded configuration file at ./cfg/.floop"
+        #default configuration
+        cfg = {
+            "port"      : "/dev/ttyUSB0",
+            "baudrate"  : 19200,
+            "parity"    : "N",
+            "stopbits"  : 1,
+            "bytesize"  : 8,
+            "timeout"   : 2
+        }
+
+        #check whether there is a configuration file lecroy.conf
+        try:
+            with open('./lecroy.conf', 'r') as f:
+                # get the configuration settings, only the port for now
+                x = f.read()
+                x = x.split('\n')
+                for i in x:
+                    s = i.rstrip().split('=')
+                    try:
+                        cfg[s[0]] = s[1]
+                        if _.isInt(s[1]):
+                            cfg[s[0]] = int(s[1])
+                    except IndexError:
+                        pass
+            print("Loaded configuration file at lecroy.conf")
+        except IOError:
+            if os.path.isfile('./lecroy.conf'):
+                print("Unable to read file. Using default configuration...")
+            else:
+                print("No configuration file found. Using default configuration...")
+
         fp = 'rawdata'
         savetime = time.strftime('%Y%m%d_%H%M')
         return self.start()
