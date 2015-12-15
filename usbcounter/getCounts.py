5f8232366eb076cd54cee546913121bcd0e957d2
Sun Yudong <sunjerry019@gmail.com>
2015-12-15 10:46:28 +0800

    fixed indents

--- usbcounter/getCounts.py
+++ usbcounter/getCounts.py
@@ -9,44 +9,44 @@ from collections import deque
 import Gnuplot, Gnuplot.PlotItems, Gnuplot.funcutils
 
 class getCounts():
-  """ Returns a generator function that outputs the counts from the usbcounter device.
-  t is the no. of data points to acquire. -1 for endless stream """
-  def __init__(self):
-    timestamp = time.strftime('%Y%m%d_%H%M')
-  def start(self,t):
-    if t == -1:
-      generator = (self.talk() for i in itertools.count(0, 1))
-    else:
-      generator = (self.talk() for i in range(t))
-    return generator
-  def talk(self):
-    proc = subprocess.Popen(['./getresponse', 'COUNTS?'], stdout = subprocess.PIPE)
-    output = proc.stdout.read()
-    if output =="timeout while waiting for response":
-      pass
-    else:
-      data = output.rstrip().split(' ')
-      data.pop(0)
-      data = [float(i) for i in data]
-      return data
-    time.sleep(0.2)
+    """ Returns a generator function that outputs the counts from the usbcounter device.
+    t is the no. of data points to acquire. -1 for endless stream """
+    def __init__(self):
+        timestamp = time.strftime('%Y%m%d_%H%M')
+    def start(self,t):
+        if t == -1:
+            generator = (self.talk() for i in itertools.count(0, 1))
+        else:
+            generator = (self.talk() for i in range(t))
+        return generator
+    def talk(self):
+        proc = subprocess.Popen(['./getresponse', 'COUNTS?'], stdout = subprocess.PIPE)
+        output = proc.stdout.read()
+        if output =="timeout while waiting for response":
+            pass
+        else:
+            data = output.rstrip().split(' ')
+            data.pop(0)
+            data = [float(i) for i in data]
+            return data
+        time.sleep(0.2)
 
 class logCounts():
-  """ Uses getCounts() to save the counts into a folder (./raw), into a ASCII file that is timestamped. set plot to True for a live plot."""
-  def check_dir(self, directory):
-    if not os.path.exists(directory):
-      os.makedirs(directory)
-  def __init__(self,t, plot = False):
-    filepath = 'raw'
-    self.check_dir(filepath)
-    timestamp = time.strftime('%Y%m%d_%H%M')
-    if t < 1:
-      raise ValueError("Infinite stream not allowed for logging")
-    with open(os.path.join(filepath, timestamp), 'w') as f:
-      gen = getCounts()
-      gen = gen.start(t)
-      for i in gen:
-        f.write("{}\t{}\n".format(i[0], i[1]))
+    """ Uses getCounts() to save the counts into a folder (./raw), into a ASCII file that is timestamped. set plot to True for a live plot."""
+    def check_dir(self, directory):
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+    def __init__(self,t, plot = False):
+        filepath = 'raw'
+        self.check_dir(filepath)
+        timestamp = time.strftime('%Y%m%d_%H%M')
+        if t < 1:
+            raise ValueError("Infinite stream not allowed for logging")
+        with open(os.path.join(filepath, timestamp), 'w') as f:
+            gen = getCounts()
+            gen = gen.start(t)
+            for i in gen:
+                f.write("{}\t{}\n".format(i[0], i[1]))
 class plotCounts():
     """ Uses getCounts() to plot the counts. Uses a temp file. """
     def __init__(self):
@@ -60,7 +60,6 @@ class plotCounts():
         self.initPlot()
 
         for i in gen:
-
             det0.appendleft(i[0])
             det1.appendleft(i[1])
             det0.pop()
