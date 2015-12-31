9a70e08c99fed48db4108d47dadcba23b48890bc
depressedsheep <depressedsheep@users.noreply.github.com>
2015-12-31 18:51:35 +0800

    getHist started

--- fruitScope/getHist.py
+++ fruitScope/getHist.py
@@ -1,3 +1,14 @@
 import sys
 sys.path.insert(0, '../helpers/')
 import lecroy
+import argparse
+
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument("t", "--duration", help="Duration of acquisition", type = int)
+  args = parser.parse_args()
+  print args.t
+  scope = lecroy.Lecroy()
+  
+  
+main()
