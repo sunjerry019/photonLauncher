bce5cce31a4d13b1c64442f62cefcd87b14577a1
Sun Yudong <sunjerry019@gmail.com>
2015-12-31 19:13:19 +0800

    proof of concept oscilloscope measurement for x duration

--- fruitScope/colonoscope.py
+++ fruitScope/colonoscope.py
@@ -2,13 +2,18 @@ import sys
 sys.path.insert(0, '../helpers/')
 import lecroy
 import argparse
+import threading
 
 def main():
-  parser = argparse.ArgumentParser()
-  parser.add_argument("t", metavar = "duration", help="Duration of acquisition", type = int, nargs = '+')
-  args = parser.parse_args()
-  print args.t
-  scope = lecroy.Lecroy()
-  
-  
+    parser = argparse.ArgumentParser()
+    parser.add_argument("t", metavar = "duration", help="Duration of acquisition", type = int, nargs = '+')
+    args = parser.parse_args()
+    print args.t
+    scope = lecroy.Lecroy()
+    def stop():
+        scope.stop()
+        scope.getHistogram()
+    scope.start()
+    t = threading.Timer(args.t[0], stop)
+    t.start()
 main()
