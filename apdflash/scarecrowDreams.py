b7828848be9b50425c2ec684809d91b80f25e7a1
Sun Yudong <sunjerry019@gmail.com>
2016-01-01 22:27:40 +0800

    init sc

--- apdflash/scarecrowDreams.py
+++ apdflash/scarecrowDreams.py
@@ -1,3 +1,21 @@
-print "hellow world"
+"""
+Rotates the stage by a certain amount with arguments rather similar to odin.py, just that the data collection is done using an oscilloscope for a set period of time.
+
+-zy
+1 Jan 2016
+
+"""
+
+
+import sys, os
+sys.path.insert(0,'../helpers/')
 
 from mpi4py import MPI
+
+import lecroy
+import mjolnir
+
+import argparse
+import math
+
+
