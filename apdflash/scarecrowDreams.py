105a176563377927fd14a9b3412eeee177aa9ab1
Sun Yudong <sunjerry019@gmail.com>
2016-01-01 13:16:25 +0800

    Reduced scarecrow for debug

--- apdflash/scarecrowDreams.py
+++ apdflash/scarecrowDreams.py
@@ -1,13 +1,5 @@
 import sys,os
 sys.path.insert(0, '../helpers')
 
-import lecroy
-import mjolnir
-
-import argparse
-
 from mpi4py import MPI
 
-comm = MPI.COMM_WORLD
-rank = comm.Get_rank()
-print rank
