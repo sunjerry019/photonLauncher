d39c82bdeb54003b5f86c152c8af5c8479b044d8
Sun Yudong <sunjerry019@gmail.com>
2016-01-02 11:15:51 +0800

    test.py doesn't work

--- apdflash/test.py
+++ apdflash/test.py
@@ -4,8 +4,10 @@ from mpi4py import MPI
 comm =  MPI.COMM_WORLD
 rank =  comm.Get_rank()
 
+print rank
+
 if rank == 0:
-	comm.send("a", dest = 1, tag = 0)
+	comm.send("dfdfdfdfdfdfdfdfdfdfdfa", dest = 1, tag = 0)
 if rank == 1:
 	print comm.recv(source = 0, tag = 0)
 
