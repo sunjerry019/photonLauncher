1294346c2061117d01684bf24a609869a68b7243
Sun Yudong <sunjerry019@gmail.com>
2016-01-02 11:18:39 +0800

    mpiexec execvp error..

--- apdflash/test.py
+++ apdflash/test.py
@@ -5,9 +5,3 @@ comm =  MPI.COMM_WORLD
 rank =  comm.Get_rank()
 
 print rank
-
-if rank == 0:
-	comm.send("dfdfdfdfdfdfdfdfdfdfdfa", dest = 1, tag = 0)
-if rank == 1:
-	print comm.recv(source = 0, tag = 0)
-
