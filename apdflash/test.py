c59bc5fbf27b8554f8b30925ae77f0c623c9d620
Sun Yudong <sunjerry019@gmail.com>
2016-01-02 12:39:37 +0800

    asdfdjifdjfd

--- apdflash/test.py
+++ apdflash/test.py
@@ -5,12 +5,13 @@ comm =  MPI.COMM_WORLD
 rank =  comm.Get_rank()
 name = MPI.Get_processor_name()
 print rank
-
+print name
 if rank == 0:
 	data = {1: 2, 3:4}
 #	print data
 	comm.send(data, dest = 1, tag = 0)
 #	print data
 if rank == 1:
+	print 'asdfD'
 	data = comm.recv(source = 0, tag = 0)
 	print data
