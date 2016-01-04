5fa3982594a0774034f25a0b9e0751bfe30079e2
Choong Zheng Yang <zhengyangchoong40@gmail.com>
2016-01-04 17:19:08 +0800

    exportHist

---
+++ fruitScope/exportHist.py
@@ -0,0 +1,24 @@
+import sys
+sys.path.insert(0, '../helpers/')
+import lecroy
+import argparse
+import json
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("f", metavar = "exportFile", help="Name of exported file", type = str)
+    args = parser.parse_args()
+
+    scope = lecroy.Lecroy()
+    print "Stopping scope"
+    scope.stop()
+    print "Getting Histogram"
+    hist = scope.getHistogram()
+
+    try:
+        with open(args.f, "w") as f:
+            json.dump(hist, f)
+            print "Saved"
+    except:
+        print "Error saving file. Is the location writable?"
+main()
