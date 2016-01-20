a6511be33c59fa5d95cdbd20130d818f7687938c
Choong Zheng Yang <zhengyangchoong40@gmail.com>
2016-01-20 13:32:20 +0800

    option to log waveform

--- fruitScope/exportHist.py
+++ fruitScope/exportHist.py
@@ -7,18 +7,23 @@ import json
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("f", metavar = "exportFile", help="File path to save in", type = str)
+    parser.add_argument("t", metavar = "type", type = str, help = "h for histogram, w for waveform")
     parser.add_argument("c", metavar = "channel", help = "1,2,3,4, A,B,C,D")
     args = parser.parse_args()
 
     scope = lecroy.Lecroy()
     print "Stopping scope"
     scope.stop()
-    print "Getting Histogram"
-    (hist,mdata) = scope.getHistogram(args.c)
+    if args.t == "h":
+        print "Getting Histogram"
+        (data,mdata) = scope.getHistogram(args.c)
+    elif args.t == "w":
+        print "Getting Waveform"
+        (data, mdata) = scope.getWaveform(args.c)
 
     try:
         with open(args.f, "w") as f:
-            for i in hist:
+            for i in data:
                 f.write('{}\t{}\n'.format(i[0],i[1]))
     except:
         print "Error saving file. Is the location writable?"
