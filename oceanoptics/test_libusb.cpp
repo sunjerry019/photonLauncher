e1c33049fa790448a553fcfdb99a3e992ac8db11
depressedsheep <depressedsheep@users.noreply.github.com>
2016-01-25 07:23:27 +0800

    can probably compile and run

--- oceanoptics/test_libusb.cpp
+++ oceanoptics/test_libusb.cpp
@@ -30,3 +30,41 @@ int main(){
 	return 0;
 
 }
+void printdev(libusb_device *dev) {
+	libusb_device_descriptor desc;
+    int r = libusb_get_device_descriptor(dev, &desc);
+    if (r < 0) {
+		cout<<"failed to get device descriptor"<<endl;
+		return;
+    };
+
+	cout<<"Number of possible configurations: "<<(int)desc.bNumConfigurations<<"  ";
+	cout<<"Device Class: "<<(int)desc.bDeviceClass<<"  ";
+	cout<<"VendorID: "<<desc.idVendor<<"  ";
+	cout<<"ProductID: "<<desc.idProduct<<endl;
+	libusb_config_descriptor *config;
+	libusb_get_config_descriptor(dev, 0, &config);
+	cout<<"Interfaces: "<<(int)config->bNumInterfaces<<" ||| ";
+	const libusb_interface *inter;
+	const libusb_interface_descriptor *interdesc;
+	const libusb_endpoint_descriptor *epdesc;
+	for(int i=0; i<(int)config->bNumInterfaces; i++) {
+		inter = &config->interface[i];
+		cout<<"Number of alternate settings: "<<inter->num_altsetting<<" | ";
+		for(int j=0; j<inter->num_altsetting; j++) {
+			interdesc = &inter->altsetting[j];
+			cout<<"Interface Number: "<<(int)interdesc->bInterfaceNumber<<" | ";
+			cout<<"Number of endpoints: "<<(int)interdesc->bNumEndpoints<<" | ";
+			for(int k=0; k<(int)interdesc->bNumEndpoints; k++) {
+				epdesc = &interdesc->endpoint[k];
+				cout<<"Descriptor Type: "<<(int)epdesc->bDescriptorType<<" | ";
+				cout<<"EP Address: "<<(int)epdesc->bEndpointAddress<<" | ";
+
+			}
+		}
+	}
+	cout<<endl<<endl<<endl;
+	libusb_free_config_descriptor(config);
+
+}
+
