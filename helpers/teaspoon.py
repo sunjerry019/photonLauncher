db9b2fd8219a96c3dac872dc2bba5a5d76a5ae0d
Choong Zheng Yang <zhengyangchoong40@gmail.com>
2016-04-06 00:02:16 +0800

    started teaspoon in helpers for tsp01

---
+++ helpers/teaspoon.py
@@ -0,0 +1,49 @@
+"""
+parser for data dump from tsp01 thorlabs application
+tsp01 is a temperature and humidty sensor
+
+we assume an external temperature probe (th1) that is attached
+
+call from command line / import the function
+
+this probably belongs inside the helper folder
+
+zy
+5 apr 2016
+"""
+
+import os, json, sys
+import argparse
+import time
+import datetime
+
+def getac(x):
+    return x.split(":\\t")[1]
+
+def getMetadata(s):
+    for i in s:
+        if "Date" in i:
+            d = getac(i)
+        if "Time (" in i:
+            t = getac(i)
+        if "Number of S" in i:
+            N = getac(i)
+    print(time.strptime(d + t,'%Y-%m-%d%H:%M:%S'))
+    #print(date + " " + time)
+
+def parse(filepath):
+    with open(filepath, 'rb') as f:
+        raw_text = str(f.read())
+        raw_text = raw_text.split('\\r\\n')
+        #print(raw_text)
+        getMetadata(raw_text)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("fp", type = str, help = "Path to textfile to be parsed.")
+    args = parser.parse_args()
+    parse(args.fp)
+
+if __name__ == '__main__':
+    main()
