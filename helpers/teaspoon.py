4896a126ac75d4b1f531559bbb03db02c992616c
Choong Zheng Yang <zhengyangchoong40@gmail.com>
2016-04-08 08:02:31 +0800

    readme file for teaspoon

--- helpers/teaspoon.py
+++ helpers/teaspoon.py
@@ -1,16 +1,16 @@
-"""
-parser for data dump from tsp01 thorlabs application
-tsp01 is a temperature and humidty sensor
 
-we assume an external temperature probe (th1) that is attached
+#parser for data dump from tsp01 thorlabs application
+#tsp01 is a temperature and humidty sensor
 
-call from command line / import the function
+#we assume an external temperature probe (th1) that is attached
 
-this probably belongs inside the helper folder
+#call from command line / import the function
 
-zy
-5 apr 2016
-"""
+#this probably belongs inside the helper folder
+
+#zy
+#5 apr 2016
+
 
 import os, json, sys
 import argparse
@@ -18,9 +18,11 @@ import time
 import datetime
 
 def getac(x):
+    """Shortcut to return the second value of a data line. """
     return x.split(":\\t")[1]
 
 def getMetadata(s):
+    """Returns the measurement interval and duration as a dictionary."""
     for i in s:
         if "Date" in i:
             d = getac(i)
@@ -40,11 +42,16 @@ def getMetadata(s):
     #print(date + " " + time)
 
 def parseline(s):
-
+    """ Returns the temperature / humidity data as a list, removing the last two lines as they are not yet used. """
     line = (s.split("\\t"))[:-2]
     line = [float(x.strip()) for x in line]
     return line
+
+
+# the data is organised in columns as follows
+# time in seconds   onboard temp sensor     onboard rel humidity sensor     external temperature probe 1    external temperature probe 2
 def parse(filepath):
+    """ Returns a tuple of data, as well as metadata. """
     with open(filepath, 'rb') as f:
         raw_text = str(f.read())
         raw_text = raw_text.split('\\r\\n')
@@ -58,10 +65,11 @@ def parse(filepath):
             if "Time [s]" in i:
                 parse_data = True
         data.pop()
-        print(data)
+        return(data, metadata)
+
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("fp", type = str, help = "Path to textfile to be parsed.")
+    """ argparse to process CLI options in case we want to use this directly, instead of using a separate controller script."""
+    parser.add_argument("fp", type = str, help = "Path to textfile from Thorlabs TSP01 to be parsed.")
     args = parser.parse_args()
     parse(args.fp)
 
