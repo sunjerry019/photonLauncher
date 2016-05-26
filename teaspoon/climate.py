d2901d1ed9a395b5c1fb2a1d388a8834805efbe7
Choong Zheng Yang <zhengyangchoong40@gmail.com>
2016-05-26 21:09:01 +0800

    added saving to file

--- teaspoon/climate.py
+++ teaspoon/climate.py
@@ -8,6 +8,7 @@ def init():
     parser = argparse.ArgumentParser()
     parser.add_argument("d", metavar = "duration", type = int, help = "Duration of acquisition and measurement in seconds. Set to -1 for infinite loop.")
     parser.add_argument("t", metavar = "time interval", type = int, help = "Time interval between each acquisition in seconds.")
+    parser.add_argument("--r", metavar = "log data", type = bool, default = False, help = "Set to true to log data.")
     args = parser.parse_args()
     total = args.d
     dt = args.t
@@ -17,8 +18,10 @@ def main(total, dt):
     teaspoon = Teaspoon()
     print(teaspoon.test() == "0")
     print len(teaspoon.test())
+    f = open("test", "w")
     for i in range(total/dt):
-        # print(teaspoon.getTemperature())
+         x = (teaspoon.getTemperature())
+         f.write("".format(x[0], x[1], teaspoon.getHumidity()))
         time.sleep(dt)
 
 
