7469d27d502f84a90d9403f874ff080fc51d0f4d
Choong Zheng Yang <zhengyangchoong40@gmail.com>
2016-05-26 09:42:00 +0800

    try get.py for teaspoon to check temperature logging; write stuff for measurement later

---
+++ teaspoon/get.py
@@ -0,0 +1,29 @@
+# wrote this in python3 oops
+# install pyusb from pip
+# install python-usbtmc from their github
+# write the udev rules
+# test script
+
+import usbtmc
+
+class Teaspoon():
+    def __init__(self):
+        self.spoon = usbtmc.Instrument(0x1313, 0x80f8)
+
+    def ping(self, x):
+        self.spoon.ask(x)
+
+    def test(self):
+        print(self.ping("*IDN?"))
+        self.getTemperature()
+
+    def getTemperature(self):
+        print(self.ping("SENS:TEMP:DATA?"))
+        print(self.ping("SENS:HUM:DATA?"))
+
+def main():
+    tea = Teaspoon()
+    tea.test()
+
+
+main():
