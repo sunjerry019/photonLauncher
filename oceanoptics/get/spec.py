b0972ad1a522c3f17cb581f44c1cdab8ea045205
Choong Zheng Yang <zhengyangchoong40@gmail.com>
2016-06-02 16:14:25 +0800

    cmd for liveplot interaction

---
+++ oceanoptics/get/spec.py
@@ -0,0 +1,30 @@
+import sys
+sys.path.insert(0, "../..helpers")
+from getSpectra import Icecube
+import cmd
+import time
+from threading import Thread
+
+class specterm(cmd.Cmd):
+    def do_start(self, line):
+        self.cube = Icecube()
+        self.spec = 0
+        self.cube.setIntegrationTime(10)
+        t = Thread(target=self.specplot)
+        t.start()
+
+    def specplot(self):
+        while True:
+            self.spec = self.cube.getSpectra()
+            with open('.temp', 'w') as f:
+                for i in self.spec:
+                    f.write("{}\t{}\n".format(i[0], i[1]))
+            time.sleep(1)
+
+    def do_EOF(self, line):
+        print("\n Exiting specterm prompt.")
+        self.cube.__exit__()
+
+        return True
+if __name__ == "__main__":
+    specterm().cmdloop()
