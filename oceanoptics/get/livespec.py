1450639b8b56c17ff1c676b8e6ad1dc56e4299c1
Choong Zheng Yang <zhengyangchoong40@gmail.com>
2016-06-08 21:26:16 +0800

    livespec should work nicely now

--- oceanoptics/get/livespec.py
+++ oceanoptics/get/livespec.py
@@ -1,23 +1,33 @@
 import sys
 sys.path.insert(0, '../../helpers/')
-
 from getSpectra import Icecube
-import time
+from time import sleep
+from threading import Thread
+import subprocess
 
-with Icecube() as cube:
-    cube.setIntegrationTime(10)
-    while True:
-        try:
-            st = time.time()
-            spec = cube.getSpectra()
-            with open('.temp', 'w') as f:
-                for i in spec:
-                    f.write("{}\t{}\n".format(i[0], i[1]))
-            print time.time() - st
-            time.sleep(1)
-        except KeyboardInterrupt:
-            print "Saving..."
-            cube.__exit__()
-#            with open("{}.dat".format(time.strftime("%M%S")) as g:
-#                for _ in spec:
-#                    g.write("{}\t{}\n".format(_[0], _[1]))
+
+def gen():
+    with Icecube() as cube:
+        cube.setIntegrationTime(10)
+        while True:
+            try:
+                spec = cube.getSpectra()
+                with open('.temp', 'w') as f:
+                    for i in spec:
+                        f.write("{}\t{}\n".format(i[0], i[1]))
+                sleep(1)
+            except KeyboardInterrupt:
+                print " --- EXITING ---"
+                cube.__exit__()
+                with open("{}.dat".format(time.strftime("%M%S")) as g:
+                    for _ in spec:
+                        g.write("{}\t{}\n".format(_[0], _[1]))
+def plot():
+    subprocess.call(["gnuplot", "loop.gnu"])
+
+t = Thread(target=gen)
+u = Thread(target=plot)
+
+t.start()
+sleep(0.1)
+u.start()
--- oceanoptics/get/spec.py
+++ oceanoptics/get/spec.py
@@ -4,16 +4,22 @@ from getSpectra import Icecube
 import cmd
 import time
 from threading import Thread
+import subprocess
 
 class specterm(cmd.Cmd):
     def do_start(self, line):
+        self.log = False
         self.cube = Icecube()
         self.spec = 0
         self.cube.setIntegrationTime(10)
-        t = Thread(target=self.specplot)
+        t = Thread(target=self.specgen)
+        u = Thread(target=self.plot)
         t.start()
-
-    def specplot(self):
+        time.sleep(0.1)
+        u.start()
+    def plot(self):
+        subprocess.call(["gnuplot", "loop.gnu"])
+    def specgen(self):
         while True:
             print "\r{}".format(time.strftime("%D"))
             self.spec = self.cube.getSpectra()
@@ -21,11 +27,6 @@ class specterm(cmd.Cmd):
                 for i in self.spec:
                     f.write("{}\t{}\n".format(i[0], i[1]))
             time.sleep(1)
-
-    def do_capture(self,fp="/mnt/photonics/spectra/".format("%Y%m%d_%H%M"), n = 1):
-        self.spec = self.cube.getSpectra()
-        with open(fp):
-
 
     def do_EOF(self, line):
         print("\n Exiting specterm prompt.")
