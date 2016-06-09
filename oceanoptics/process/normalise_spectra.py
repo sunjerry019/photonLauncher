0ff0c4b9ada4c496f61840c3b8a368100e1e5991
Choong Zheng Yang <zhengyangchoong40@gmail.com>
2016-06-10 00:27:15 +0800

    output option for norm spectra

--- oceanoptics/process/normalise_spectra.py
+++ oceanoptics/process/normalise_spectra.py
@@ -7,7 +7,7 @@ import numpy as np
 import math
 
 class norm():
-    def __init__(self, basefile, samplefile, ntype, raw = True):
+    def __init__(self, basefile, samplefile, ntype, raw = True, outpath = ''):
         base = []
         sample = []
         result = []
@@ -20,7 +20,11 @@ class norm():
             elif ntype == 'sub':
                 result.append([base[i][0], (sample[i][1] - base[i][1]), self.getSubErrorOf([base[i][1], base[i][2]],[sample[i][1], sample[i][2]])])
         if raw:
-            with open(join(dirname(samplefile), "norm{}_{}".format(basename(sample), basename(basefile)))) as f:
+            if not outpath == "":
+                fp = outpath
+            else:
+                fp = "./normspectra{}".format(time.strftime("%Y%m%d_%H%M"))
+            with open(fp) as f:
                 for i in result:
                     f.write("{}\t{}\t{}\n".format(i[0], i[1], i[2]))
         else:
@@ -53,6 +57,7 @@ if __name__ == '__main__':
     parser.add_argument('ntype', type = str, help = "Normalise by division or subtraction. 'div' and 'sub' respectively")
     #parser.add_argument('-d', '--dataformat', type = str, help = "Defaults to raw, choose json to process.json files", default = "raw")
     parser.add_argument('-r', '--rawfile', action = 'store_true', help = "True to output raw, plottable ascii file", default = None)
+    parser.add_argument('-o', '--outputfile', type = str, help = "Filepath to output. ", default = None)
     args = parser.parse_args()
 
-    n = norm(args.bn, args.sn, args.ntype, raw = args.rawfile,)
+    n = norm(args.bn, args.sn, args.ntype, raw = args.rawfile, output = args.outputfile )
