d2530500c7c2673e44c8beccd8e97f1128ebec93
Choong Zheng Yang <zhengyangchoong40@gmail.com>
2016-07-18 15:11:07 +0800

    Added argparse

--- roger/roger.py
+++ roger/roger.py
@@ -1,13 +1,21 @@
 import serial
 import time
 import sys
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("n", type = int, help = "No. of readings to take")
+parser.add_argument("-f", "--log", action = store_true, help = "Use this flag to enable logging to a file.")
+args = parser.parse_args()
+
+
 
 light = serial.Serial(port = '/dev/ttyACM0',baudrate = 19200,parity = 'N',stopbits = 1,bytesize = 8)
 ##light.open()
 
-log = True
+log = args.log
 
-c = 10e5
+c = args.n
 if log:
     f = open("photometer_{}".format(time.strftime("%Y%m%d_%H%M")), 'w')
 
@@ -16,7 +24,7 @@ while True:
         x = light.readline()
         x = x.strip()
         x = float(x)
-        sys.stdout.flush()        
+        sys.stdout.flush()
         sys.stdout.write("\r{}".format(x))
         if log:
             f.write("{}\n".format(x))
