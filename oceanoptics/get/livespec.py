6e5f084345804615f673e4748f69ed25fbb79d2c
Choong Zheng Yang <zhengyangchoong40@gmail.com>
2016-07-27 08:04:32 +0800

    asdf

--- oceanoptics/get/livespec.py
+++ oceanoptics/get/livespec.py
@@ -10,41 +10,39 @@ import time
 
 def main(n, plot):
     def gen(n):
-        cube = Icecube()
-
-        cube.setIntegrationTime(10)
         foldername = time.strftime("%Y%m%d_%H%M")
         if not n == -1:
             os.mkdir(foldername)
-        while True:
-            try:
-		print n
-                spec = cube.getSpectra()
-                with open('.temp', 'w') as f:
-                    for i in spec:
-                        f.write("{}\t{}\n".format(i[0], i[1]))
+        with cube as Icecube():
+            while True:
+                try:
+                    spec = cube.getSpectra()
 
-                sleep(1)
-                n -= 1
-
-                if n == 0:
-                    break
-                elif n > 0:
-                    with open("{}/data_{}".format(foldername,n) , 'w') as f:
+                    with open('.temp', 'w') as f:
                         for i in spec:
                             f.write("{}\t{}\n".format(i[0], i[1]))
-                            
-            except KeyboardInterrupt:
-                break
-                print " --- EXITING ---"
-                cube.__exit__()
-                with open("{}.dat".format(time.strftime("%M%S"))) as g:
-                    for _ in spec:
-                        g.write("{}\t{}\n".format(_[0], _[1]))
-		sys.exit()
+
+                    sleep(1)
+                    n -= 1
+
+                    if n == 0:
+                        break
+                    elif n > 0:
+                        with open("{}/data_{}".format(foldername,n) , 'w') as f:
+                            for i in spec:
+                                f.write("{}\t{}\n".format(i[0], i[1]))
+
+                except KeyboardInterrupt:
+                    break
+                    print " --- EXITING ---"
+                    cube.__exit__()
+                    with open("{}.dat".format(time.strftime("%M%S"))) as g:
+                        for _ in spec:
+                            g.write("{}\t{}\n".format(_[0], _[1]))
     def plot():
         subprocess.call(["gnuplot", "loop.gnu"])
 
+    
     t = Thread(target=gen, args=(n,))
     u = Thread(target=plot)
 
