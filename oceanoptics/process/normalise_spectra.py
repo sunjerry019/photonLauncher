3242b76c019c1f73d0e39f8c1212556d7232086c
Choong Zheng Yang <zhengyangchoong40@gmail.com>
2016-07-27 16:38:02 +0800

    ashdfjkl

--- oceanoptics/process/normalise_spectra.py
+++ oceanoptics/process/normalise_spectra.py
@@ -7,28 +7,37 @@ import numpy as np
 import math
 
 class norm():
-    def __init__(self, basefile, samplefile, ntype, raw = True, output = ''):
+    def __init__(self, basefile, samplefile, ntype, output = ''):
         base = []
-        sample = []
+
         result = []
         base = self.parse_raw(basefile,base)
-        sample = self.parse_raw(samplefile,sample)
+        #sample = []
+        #sample = self.parse_raw(samplefile,sample)
+        #print max(sample)
+        print max(base)
 
+        normalise_range = (400, 800)
         for i in xrange(len(base)):
+            if not(base[i][0] >= normalise_range[0] and base[i][0] <= normalise_range[1]):
+                continue
+            # 0 is the index/ wavelength, 1 is for the counts at that wavelength, 2 is the yerror at the wavelength
             if ntype == 'div':
-                result.append([base[i][0], (sample[i][1] / base[i][1]), self.getDivErrorOf([base[i][1], base[i][2]],[sample[i][1], sample[i][2]])])
+                result.append([base[i][0], (sample[i][1] / base[i][1]),  self.getDivErrorOf([base[i][1], base[i][2]],[sample[i][1], sample[i][2]])])
             elif ntype == 'sub':
                 result.append([base[i][0], (sample[i][1] - base[i][1]), self.getSubErrorOf([base[i][1], base[i][2]],[sample[i][1], sample[i][2]])])
-        if raw:
-            if not output == "":
-                fp = output
-            else:
-                fp = "./normspectra{}".format(time.strftime("%Y%m%d_%H%M"))
-            with open(fp, "w") as f:
-                for i in result:
-                    f.write("{}\t{}\t{}\n".format(i[0], i[1], i[2]))
+            elif ntype == 'norm':
+                pass
+
+
+        if not output == "":
+            fp = output
         else:
-            print result
+            fp = "./normspectra{}".format(time.strftime("%Y%m%d_%H%M"))
+        with open(fp, "w") as f:
+            for i in result:
+                f.write("{}\t{}\t{}\n".format(i[0], i[1], i[2]))
+
         print " == Normalisation Complete == \n"
 
     def parse_raw(self,_f, data_array):
@@ -53,11 +62,9 @@ class norm():
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('bn', type = str, help = "Path to base file")
-    parser.add_argument('sn', type = str, help = "Path to of sample file")
-    parser.add_argument('ntype', type = str, help = "Normalise by division or subtraction. 'div' and 'sub' respectively")
-    #parser.add_argument('-d', '--dataformat', type = str, help = "Defaults to raw, choose json to process.json files", default = "raw")
-    parser.add_argument('-r', '--rawfile', action = 'store_true', help = "True to output raw, plottable ascii file", default = None)
-    parser.add_argument('-o', '--outputfile', type = str, help = "Filepath to output. ", default = None)
+    parser.add_argument('-sn', '--samplefile', type = str, help = "Path to of sample file", default = "")
+    parser.add_argument('ntype', type = str, help = "Normalise by division or subtraction. 'div' and 'sub' respectively. This operates by performing the function from the sample file to the base file. ")
+    parser.add_argument('-o', '--outputfile', type = str, help = "Filepath to output. ")
     args = parser.parse_args()
 
-    n = norm(args.bn, args.sn, args.ntype, raw = args.rawfile, output = args.outputfile )
+    n = norm(args.bn, args.samplefile, args.ntype, output = args.outputfile )
