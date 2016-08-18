774b493040fbb40ba07cc165afd81a3fed0b235f
Choong Zheng Yang <zhengyangchoong40@gmail.com>
2016-08-19 07:13:15 +0800

    Added raw dump

--- spectrometer/gr8.py
+++ spectrometer/gr8.py
@@ -6,20 +6,12 @@ import numpy as np
 import time
 kek = Mjolnir()
 
-#kek.homeMotor()
-
-#kek.moveRotMotor(356.5)
-
-
-#time.sleep(100)
-
 kek.moveRotMotor(5)
-#kek.moveRotMotor(-10)
-
-#time.sleep(100)
 light = serial.Serial(port = '/dev/ttyACM0', baudrate=19200,parity='N',stopbits=1,bytesize=8)
 data = []
-f  = open("{}".format(time.strftime("%Y%m%d_%H%M")), "w")
+fn = time.strftime("%Y%m%d_%H%M"))
+f  = open("{}".format(fn, "w")
+g = open("rawspec_{}".format(fn), "w")
 for j in xrange(1000):
     try:
         kek.moveRotMotor(-0.01)
@@ -36,22 +28,19 @@ for j in xrange(1000):
         data.append(np.array(zz))
 
     except KeyboardInterrupt:
-        #print "writing"
-        #c = 0
-        #for i in data:
-        #    f.write("{}\t{}\t{}\n".format(c, np.mean(i), np.std(i)))
-        #    c += 1
-        break 
-    #print i
-#data = np.array(data)
+        break
 
-#f = open("{}".format(time.strftime("%Y%m%d_%H%M")), 'w')
 print "writing to file"
 
 c = 0
 for i in data:
-    
     f.write("{}\t{}\t{}\n".format(c, np.mean(i), np.std(i)))
     c += 1
-
+c = 0
+for j in data:
+    g.write("{}\t".format(c))
+    for k in j:
+        g.write("{}\t".format(k))
+    g.write("\n")
+    c += 1
 kek.moveRotMotor(5)
