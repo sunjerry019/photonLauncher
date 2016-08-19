7a2fdf0ef2111e89c9da7abcfb5b300671166423
Choong Zheng Yang <zhengyangchoong40@gmail.com>
2016-08-19 08:29:40 +0800

    gui display

--- roger/roger.py
+++ roger/roger.py
@@ -2,6 +2,17 @@ import serial
 import time
 import sys
 import argparse
+from Tkinter import *
+
+root = Tk()
+root.resizable(width = False, height= False)
+
+power = StringVar()
+power.set('Arbitrary Power: {}'.format(0))
+
+l = Label(root, textvariable=var, font("Helvetica", 16))
+l.pack()
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("n", type = int, help = "No. of readings to take")
@@ -27,13 +38,12 @@ while True:
         x = x.strip()
         x = float(x)
         if c % 5000 == 0:
-            sys.stdout.flush()
-            sys.stdout.write("\r power reading: {}".format(x))
-#            prevtime = time.time()
+            power.set('Power: {}'.format(x))
+            root.update_idletasks()
         if log:
             f.write("{}\n".format(x))
-#        time.sleep(0.05)
-        if c< 0:
+
+        if c < 0:
             break
         else:
             c-=1
