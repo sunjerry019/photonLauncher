a49273663d2480ac6fd393d054d3f7f64d3b00c5
Sun Yudong <sunjerry019@gmail.com>
2019-03-18 16:45:30 +0800

    add EEPROM to getSpectra

--- helpers/getSpectra.py
+++ helpers/getSpectra.py
@@ -2,15 +2,77 @@ import binascii
 import struct
 import usb1
 import os
+from collections import deque
 
 class Icecube():
 
-    def parseWavelengths(self,x):
+    def parseWavelengths(self, x):
         with open(x) as f:
             text = f.read()
-        text=text.strip().split(",")
+        text = text.strip().split(",")
         wavelengths = [float(y) for y in text]
         return wavelengths
+
+    def getEEPROM(self):
+        # READ EEPROM
+        """
+            31 EEPROM values
+            0  - Serial Number
+            1  - 0th order Wavelength Calibration Coefficient
+            2  - 1st order Wavelength Calibration Coefficient
+            3  - 2nd order Wavelength Calibration Coefficient
+            4  - 3rd order Wavelength Calibration Coefficient
+            5  - Stray Light Constant
+            6  - 0th order non-linearity correction coefficient
+            7  - 1st order non-linearity correction coefficient
+            8  - 2nd order non-linearity correction coefficient
+            9  - 3rd order non-linearity correction coefficient
+            10 - 4th order non-linearity correction coefficient
+            11 - 5th order non-linearity correction coefficient
+            12 - 6th order non-linearity correction coefficient
+            13 - 7th order non-linearity correction coefficient
+            14 - Polynomial order of non-linearity calibration
+            15 - Optical Bench Configuration: gg ff sss
+                gg  = Grating #
+                fff = filter wavelengths
+                sss = slit size
+                https://oceanoptics.com/product-details/qe-pro-custom-configured-gratings-and-wavelength-range/
+            16 - USB4000 configurations: AWL V
+                A = Array coating Mfg
+                W = Array wavelength (VIS, UV, OFLV)
+                L = L2 Lens installed
+                V = CPLD Version
+            17 - Autonulling information
+            18 - Power-up baud Rate Value
+            19-30 - User-configured
+        """
+        _eeprom = list()
+        for i in xrange(0, 31):
+            # print "Reading Byte " + str(i)
+            self.icecube.bulkWrite(1, '\x05' + chr(i))
+            data = self.icecube.bulkRead(1, 17)
+            # bytes = [data[i:i+2] for i in xrange(0, len(data), 2)]
+            retCount = len(data)
+            # unpacked = struct.unpack('<' + str(retCount) +'c', data)
+            # if len(unpacked) >= 2: unpacked = unpacked[2:]
+            # unpacked = deque(unpacked)                          # Change the format to deque to pop from front
+            unpacked = deque(struct.unpack('<' + str(retCount) +'c', data))
+            if len(unpacked) > 2: unpacked.popleft(); unpacked.popleft();      # We discard the \x05 and Byte Number
+            _u = list()
+
+            while unpacked and unpacked[0] != b'\x00':                         # Discard everything after \x00
+                _u.append(unpacked.popleft())
+
+            try:                                                               # Decode to ascii if possible
+                _u = "".join(_u).decode('ascii')
+                _u = float(_u)
+            except (UnicodeDecodeError, ValueError), e:
+                pass
+
+            _eeprom.append(_u)
+
+        return _eeprom
+
 
     def getTemp(self):
         self.icecube.bulkWrite(1, '\x6C')
