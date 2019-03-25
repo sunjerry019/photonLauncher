586ff46b59cebea1c31490af4e7250a4fac03187
Sun Yudong <sunjerry019@gmail.com>
2019-03-25 16:25:24 +0800

    add FLAME-S

--- helpers/getSpectra.py
+++ helpers/getSpectra.py
@@ -1,11 +1,12 @@
+from __future__ import division
 import binascii
 import struct
 import usb1
 import os
 from collections import deque
+import time
 
 class Icecube():
-
     def parseWavelengths(self, x):
         with open(x) as f:
             text = f.read()
@@ -13,7 +14,11 @@ class Icecube():
         wavelengths = [float(y) for y in text]
         return wavelengths
 
-    def getEEPROM(self):
+    def getAutonulling(self):
+        _an = struct.unpack("<h", "".join(self.getEEPROM(17)[4:6]))[0]
+        return 65535/_an
+
+    def getEEPROM(self, idx = None):
         # READ EEPROM
         """
             31 EEPROM values
@@ -47,32 +52,42 @@ class Icecube():
             19-30 - User-configured
         """
         _eeprom = list()
-        for i in xrange(0, 31):
-            # print "Reading Byte " + str(i)
-            self.icecube.bulkWrite(1, '\x05' + chr(i))
-            data = self.icecube.bulkRead(1, 17)
-            # bytes = [data[i:i+2] for i in xrange(0, len(data), 2)]
-            retCount = len(data)
-            # unpacked = struct.unpack('<' + str(retCount) +'c', data)
-            # if len(unpacked) >= 2: unpacked = unpacked[2:]
-            # unpacked = deque(unpacked)                          # Change the format to deque to pop from front
-            unpacked = deque(struct.unpack('<' + str(retCount) +'c', data))
-            if len(unpacked) > 2: unpacked.popleft(); unpacked.popleft();      # We discard the \x05 and Byte Number
-            _u = list()
+        _end = {
+            "USB4000" : 31,
+            "FLAME-S" : 20
+        }
+        self.eepromEnd = _end[self.type]
+        if idx is None:
+            for i in xrange(0, self.eepromEnd):
+                _u = self.getSingleEEPROM(i)
+                _eeprom.append(_u)
+            return _eeprom
+        else:
+            return self.getSingleEEPROM(idx)
 
-            while unpacked and unpacked[0] != b'\x00':                         # Discard everything after \x00
-                _u.append(unpacked.popleft())
+    def getSingleEEPROM(self, i):
+        # print "Reading Byte " + str(i)
+        self.icecube.bulkWrite(self.endpoints["EP1OUT"], '\x05' + chr(i))
+        data = self.icecube.bulkRead(self.endpoints["EP1OUT"], 17)
+        # bytes = [data[i:i+2] for i in xrange(0, len(data), 2)]
+        retCount = len(data)
+        # unpacked = struct.unpack('<' + str(retCount) +'c', data)
+        # if len(unpacked) >= 2: unpacked = unpacked[2:]
+        # unpacked = deque(unpacked)                                       # Change the format to deque to pop from front
+        unpacked = deque(struct.unpack('<' + str(retCount) +'c', data))
+        if len(unpacked) > 2: unpacked.popleft(); unpacked.popleft();      # We discard the \x05 and Byte Number
+        _u = list()
 
-            try:                                                               # Decode to ascii if possible
-                _u = "".join(_u).decode('ascii')
-                _u = float(_u)
-            except (UnicodeDecodeError, ValueError), e:
-                pass
+        while unpacked and unpacked[0] != b'\x00':                         # Discard everything after \x00
+            _u.append(unpacked.popleft())
 
-            _eeprom.append(_u)
+        try:                                                               # Decode to ascii if possible
+            _u = "".join(_u).decode('ascii')
+            _u = float(_u)
+        except (UnicodeDecodeError, ValueError), e:
+            pass
 
-        return _eeprom
-
+        return _u
 
     def getTemp(self):
         self.icecube.bulkWrite(1, '\x6C')
@@ -93,27 +108,64 @@ class Icecube():
     def getSpectra(self):
         data = []
         _data = []
-        self.icecube.bulkWrite(1, '\x09')
-        for _ in xrange(4):
-        	data.append(self.icecube.bulkRead(134, 512))
-        for _ in xrange(11):
-        	data.append(self.icecube.bulkRead(130, 512))
-        self.icecube.bulkRead(130, 1)
+        self.icecube.bulkWrite(self.endpoints["EP1OUT"], '\x09')
+        if self.type == "USB4000":
+            for _ in xrange(4):
+            	data.append(self.icecube.bulkRead(self.endpoints["EP6IN"], 512))
+            for _ in xrange(11):
+            	data.append(self.icecube.bulkRead(self.endpoints["EP2IN"], 512))
+        else:
+            for _ in xrange(8):
+                data.append(self.icecube.bulkRead(self.endpoints["EP2IN"], 512))
+        # Synchronization Byte
+        self.icecube.bulkRead(self.endpoints["EP2IN"], 1)
 
         for j in xrange(len(data)):
             for i in xrange(256):
                 x = data[j][2*i:(i+1)*2]
                 _data.append(struct.unpack('<h', x)[0])
-        return [(self.wavelengths[i], _data[i]) for i in xrange(len(self.wavelengths))]
+        return [(self.wavelengths[i], self.autonulling * _data[i]) for i in xrange(len(self.wavelengths))]
 
     def __enter__(self):
         dir_path = os.path.dirname(os.path.realpath(__file__))
-        wavelengths = self.parseWavelengths(dir_path + "/.oceanOpticsWavelength")
         context = usb1.USBContext()
+
+        # EP1OUT, EP2IN, EP6IN, EP1IN\
+        self.endpoints = {
+            "EP1OUT": 1,
+            "EP2IN" : int("82", 16),
+            "EP6IN" : int("86", 16),
+            "EP1IN" : int("81", 16)
+        }
+
+        # Checking type
         self.icecube = context.openByVendorIDAndProductID(0x2457, 0x1022, skip_on_error=True)
+
+        if self.icecube is not None:
+            self.type = "USB4000"
+            self.autonulling = 1
+            wavelengths = self.parseWavelengths(dir_path + "/.USB4000.wv")
+        else:
+            self.icecube = context.openByVendorIDAndProductID(0x2457, 0x101e, skip_on_error=True)
+            if self.icecube is not None:
+                self.type = "FLAME-S"
+                wavelengths = self.parseWavelengths(dir_path + "/.FLAME-S.wv")
+            else:
+                print("Unable to find appropriate spectrometer")
+                exit(1)
+
         self.icecube.claimInterface(0)
-        self.icecube.bulkWrite(1, '\x01') # init message
+        self.icecube.bulkWrite(self.endpoints["EP1OUT"], '\x01') # init message
+
+        if self.type == "FLAME-S":
+            # print("Apparently we need 3 seconds to start up")
+            # time.sleep(3) # wait for it to startup
+            self.autonulling = self.getAutonulling()
+
         self.wavelengths = wavelengths
+
+        print(self.getEEPROM())
+
         return self
 
     def __exit__(self, type, value, traceback):
