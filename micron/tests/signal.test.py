7a4cc072911f9cb503e199d00470bb4cf493f54d
Sun Yudong <sunjerry019@gmail.com>
2019-06-09 19:08:45 +0800

    testscripts

---
+++ micron/tests/signal.test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import signal
+import sys
+import time
+import threading
+
+class Hi():
+	def __init__(self):
+		signal.signal(signal.SIGINT, self.signal_handler)
+
+		for i in range(5000):
+			print("Sleeping {}".format(i), end="\r")
+			time.sleep(i)
+
+	def signal_handler(self, signal, frame):
+	    print('You pressed Ctrl+C!')
+	    sys.exit(1)
+
+x = Hi()
