ca1e1556248c22aa4f437bd8852f1bab19bec947
Wu Mingsong <mingsong123@gmail.com>
2019-06-17 13:02:10 +0800

    added more sound mechano opto position wav files

---
+++ micron/guimicro.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# WMS's attempt at starting the graphical user interface effort for the benefit of future members of the Nanomaterials lab ("if only everything can be done with a single click of a single button")
+# Swapped to PyQt4 from Tkinter, because the former is more powerful/intuitive to implement features
+# microgui will pair functions with commands imported from raster.py
+
+#import raster
+import sys
+from PyQt4 import QtGui
+
+class Window(QtGui.QMainWindow):
+
+    def __init__(self):
+        super(Window, self).__init__()
+        self.setGeometry(50, 50, 500, 300)
+        self.setWindowTitle("Micos Stage Controller MARK II")
+        self.setWindowIcon(QtGui.QIcon('pythonlogo.png'))
+        self.show()
+
+
+
+app = QtGui.QApplication(sys.argv)
+GUI = Window()
+sys.exit(app.exec_())
