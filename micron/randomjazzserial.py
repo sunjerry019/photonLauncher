67077f33b50d5c74f0a19d6f2786236f3f434f1b
mingsong <you@example.com>
2019-06-24 17:59:34 +0800

    Started script on function generating random jazz score to pair with each serial device to serve as the finished cutting alarm.

---
+++ micron/randomjazzserial.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from pydub import AudioSegment
+from pydub.generators import SignalGenerator
+from pydub.generators import Sine
+from pydub.playback import play
+import random2
+
+c4, c4sharp, d4, d4sharp, e4, f4, f4sharp, g4, g4sharp, a4, a4sharp, b4, c5, c5sharp, d5, d5sharp, e5 = 261.626, 277.183, 293.665, 311.127,  329.628, 349.228, 369.994, 391.995, 415.305, 440.000, 466.164, 493.883, 523.251, 554.365, 587.330, 622.254, 659.255
+
+
+
+class RandomJazzCereal():
+
+    def __init__(self):
+        pass
+
+    def hi(self, cereal):
