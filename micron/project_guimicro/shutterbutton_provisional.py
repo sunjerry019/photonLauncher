8eb2a04ac6ddc1fa8c96a8aa72f113ff381cd20c
Wu Mingsong <mingsong123@gmail.com>
2019-07-09 23:14:58 +0800

    added status bar to indicate shutter state + some lovely stylesheeting

--- micron/project_guimicro/shutterbutton_provisional.py
+++ micron/project_guimicro/shutterbutton_provisional.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import pyqtSlot
 
@@ -14,22 +14,31 @@ class Butt(QWidget):
 
     def __init__(self):
         super().__init__()
-        self.title = 'PyQt5 button - pythonspot.com'
+        self.title = 'Optical audio shutter'
         self.left = 10
         self.top = 10
-        self.width = 320
-        self.height = 200
-        self.initUI()
+        self.width = 500
+        self.height = 500
         self.shutter = shutterpowerranger.Servo(absoluteMode = True)
         self.isOpen = False
+        self.initUI()
 
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
 
-        button = QPushButton('PyQt5 button', self)
-        button.setToolTip('This is an example button')
-        button.move(100,70)
+        self.textbox = QLabel(self)
+        self.textbox.move(100, 20)
+        self.textbox.resize(350,80)
+        self.textbox.setText("HELLO WORLD")
+        self.setStyleSheet("QLabel {font: bold 18pt Roboto}")
+
+
+        button = QPushButton('LAUNCH\nSEQUENCE', self)
+        button.setToolTip('A single button. Click it maybe?')
+        button.move(125,125)
+        button.resize(250,250)
+        button.setStyleSheet('QPushButton {background-color: red; color: black; font: bold 20pt Roboto}')
         button.clicked.connect(self.on_click)
 
         self.show()
@@ -39,9 +48,13 @@ class Butt(QWidget):
         if self.isOpen:
             self.shutter.close()
             self.isOpen = False
+            self.textbox.setText("You closed the shutter\nGood job!!")
         else:
             self.shutter.open()
             self.isOpen = True
+            self.textbox.setText("You opened the shutter\nWE'RE DOOMED!!")
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = Butt()
