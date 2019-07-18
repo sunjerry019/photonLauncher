a4462e4836bfefd127ee5d62fadfdd743f3b60be
Sun Yudong <sunjerry019@gmail.com>
2019-07-18 22:22:20 +0800

    support time estimation run

--- micron/1_runs/zhiyuan/earth.py
+++ micron/1_runs/zhiyuan/earth.py
@@ -11,9 +11,10 @@ import picConv, micron
 import datetime
 
 _velocity = 50
-_MODE = "RUDOLPH"
-_FILENAME = "Australia-NewGuinea.bmp" # , #"robinson_big.bmp", #
+_MODE = "OSCAR"
+_FILENAME = "./split_big/robinson_big2-3.bmp" #"Australia-NewGuinea.bmp" # , #"robinson_big.bmp", #
 
+_ESTIMATEONLY = False
 
 # RODULPH
 
@@ -26,9 +27,7 @@ if _MODE == "RUDOLPH":
 
     testPic.convert()
     testPic.parseLines()
-    testPic.draw(velocity = _velocity, shutterAbsolute = True)
-
-    # print(datetime.timedelta(seconds = testPic.estimateTime(velocity = _velocity))
+    testPic.draw(velocity = _velocity, shutterAbsolute = True) if not _ESTIMATEONLY else print(datetime.timedelta(seconds = testPic.estimateTime(velocity = _velocity)))
 
 # OSCAR
 elif _MODE == "OSCAR":
@@ -41,30 +40,31 @@ elif _MODE == "OSCAR":
         xscale = 0.5
     )
 
-    testPic.controller = micron.Micos(
-        stageConfig = {
-            "xlim" : [-10000,0],
-            "ylim" : [-10000,0],
-        },
-        noHome = True,
-        shutterAbsolute = True
-    )
+    if not _ESTIMATEONLY:
+        testPic.controller = micron.Micos(
+            stageConfig = {
+                "xlim" : [-10000,0],
+                "ylim" : [-10000,0],
+            },
+            noHome = True,
+            shutterAbsolute = True
+        )
 
-    testPic.controller.setvel(1000)
-    # testPic.controller.send("rm")
-    # time.sleep(2)
-    # testPic.controller.abort()
-    testPic.controller.setpos(0, 0)
+        testPic.controller.setvel(1000)
+        # testPic.controller.send("rm")
+        # time.sleep(2)
+        # testPic.controller.abort()
+        testPic.controller.setpos(0, 0)
 
-    xl = abs(testPic.controller.stage.xlim[1] - testPic.controller.stage.xlim[0])
-    yl = abs(testPic.controller.stage.ylim[1] - testPic.controller.stage.ylim[0])
+        xl = abs(testPic.controller.stage.xlim[1] - testPic.controller.stage.xlim[0])
+        yl = abs(testPic.controller.stage.ylim[1] - testPic.controller.stage.ylim[0])
 
-    testPic.controller.setlimits(testPic.controller.stage.xlim[0], testPic.controller.stage.ylim[0], testPic.controller.stage.xlim[1], testPic.controller.stage.ylim[1])
-    testPic.controller.rmove(x = -xl/2, y = -yl/2)
+        testPic.controller.setlimits(testPic.controller.stage.xlim[0], testPic.controller.stage.ylim[0], testPic.controller.stage.xlim[1], testPic.controller.stage.ylim[1])
+        testPic.controller.rmove(x = -xl/2, y = -yl/2)
 
-    testPic.controller.waitClear()
-    testPic.homed = True
+        testPic.controller.waitClear()
+        testPic.homed = True
 
     testPic.convert()
     testPic.parseLines()
-    testPic.draw(velocity = _velocity)
+    testPic.draw(velocity = _velocity) if not _ESTIMATEONLY else print(datetime.timedelta(seconds = testPic.estimateTime(velocity = _velocity)))
