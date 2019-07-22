12198a794de6b9cab23d07c669c430f843c6fb76
Sun Yudong <sunjerry019@gmail.com>
2019-07-22 15:28:19 +0800

    test right ch

--- micron/project_guimicro/shutterbtn.py
+++ micron/project_guimicro/shutterbtn.py
@@ -19,7 +19,7 @@ class Butt(QWidget):
         self.top = 10
         self.width = 500
         self.height = 500
-        self.shutter = shutterpowerranger.Shutter(absoluteMode = True)
+        self.shutter = shutterpowerranger.Shutter(absoluteMode = True, channel = shutterpowerranger.Servo.RIGHTCH)
         self.isOpen = False
         self.initUI()
 
