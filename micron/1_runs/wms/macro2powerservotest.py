486bce9268a0d5e1f7725332b1a196d18a05e17d
Wu Mingsong <mingsong123@gmail.com>
2019-08-01 17:59:35 +0800

    documentation

--- micron/1_runs/wms/macro2powerservotest.py
+++ micron/1_runs/wms/macro2powerservotest.py
@@ -5,20 +5,11 @@ sys.path.insert(0, "../../")
 
 import servos
 
+# non absolute 360 servo ND filter mark i set up: 0.07 duty, 130 duration = 0.079 duty, 200 duration)
+
 p = servos.Power(absoluteMode = False, channel = servos.Servo.RIGHTCH)
 s = servos.Shutter(absoluteMode = True, channel = servos.Servo.LEFTCH)
 
-# for i in range(0,5):
-#     p.absolute(0.072, duration = 200)
-#     time.sleep(2)
-
-# for i in range(0,5):
-#     p.absolute(0.079, duration = 200)
-#     time.sleep(2)
-
-# s.open()
-# s.close()
-
 p.absolute(0.07, duration = 130)
 time.sleep(1)
 p.absolute(0.07, duration = 130)
@@ -27,7 +18,3 @@ p.absolute(0.079, duration = 200)
 time.sleep(1)
 p.absolute(0.079, duration = 200)
 time.sleep(1)
-
-
-
-
