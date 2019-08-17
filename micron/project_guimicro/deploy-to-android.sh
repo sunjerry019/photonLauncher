c6ff9cd2d9da31db76fe96562adc129f9de36964
Sun Yudong <sunjerry019@gmail.com>
2019-08-18 04:41:34 +0800

    include all resource dependecies for compilation

--- micron/project_guimicro/deploy-to-android.sh
+++ micron/project_guimicro/deploy-to-android.sh
@@ -2,3 +2,18 @@
 
 pyqtdeploy-build shutterbtn.pdy --target android-64
 cd build-android-64
+
+# https://blog.lasconic.com/compiling-qt-android-app-with-docker/
+
+sudo docker pull lasconic/qt:5.4-android
+sudo docker run -i -t -v /home/sunyudong/git/photonLauncher/micron/project_guimicro/build-android-64:/source lasconic/qt:5.4-android
+
+
+# IN DOCKER
+mkdir ~/build
+cd ~/build
+qmake -r /source/myproject.pro
+make -j2
+mkdir ~/dist
+make install INSTALL_ROOT=~/dist
+androiddeployqt --input android-libmyproject.so-deployment-settings.json --output ~/dist --name myapp --deployment bundled --gradle --release
