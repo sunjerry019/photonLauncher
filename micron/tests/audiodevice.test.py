a70d53cc006ffccda0edb42693281f8ffaba2a89
Sun Yudong <sunjerry019@gmail.com>
2019-09-18 01:29:29 +0800

    audio test

--- micron/tests/audiodevice.test.py
+++ micron/tests/audiodevice.test.py
@@ -9,7 +9,10 @@ from pwmaudio import noALSAerror
 with noALSAerror():
     p = pyaudio.PyAudio()
     info = p.get_host_api_info_by_index(0)
+    print(p.get_host_api_count())
+    print(info)
     numdevices = info.get('deviceCount')
     for i in range(0, numdevices):
             if (p.get_device_info_by_host_api_device_index(0, i).get('maxOutputChannels')) > 0:
-                print("Output Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
+                # print("Output Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
+                print("Output Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i))
